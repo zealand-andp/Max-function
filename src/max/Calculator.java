cc2699df0c10b3d50ee8aed07befc77537561ee2
Anders Petersen <AndP@easj.dk>
2019-10-08 12:46:41 +0200

    Calculator.max refactored to use Math.max

--- src/max/Calculator.java
+++ src/max/Calculator.java
@@ -2,9 +2,6 @@ package max;
 
 public class Calculator {
     public static int max(int a, int b) {
-        if (a > b)
-            return a;
-        else
-            return b;
+        return Math.max(a, b);
     }
 }
